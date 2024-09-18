d8b9c9342d47e1ba708f10afe682e1038abcae5b
jordanbrown0606 <157506730+jordanbrown0606@users.noreply.github.com>
2024-09-18 09:10:30 -0500

    Added player movement and third person camera.

--- Malcom vs the Mind/Assets/Scripts/Shooting.cs
+++ Malcom vs the Mind/Assets/Scripts/Shooting.cs
@@ -15,7 +15,7 @@ public class Shooting : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.E))
+        if (Input.GetMouseButtonDown(0))
         {
             Ray ray = _camera.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
